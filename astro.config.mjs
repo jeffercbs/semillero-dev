77de211327c52c9b1867ea455b165d28cabffd89
Jeferson Barrero <jebarrero14@gmail.com>
2024-01-20 13:02:28 -0500

    Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://semillero-dev.vercel.app",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+});
